225c859a53740b6b71b1db34c8c36d301e8899ca
Eugene <59334320+JenyaYurch@users.noreply.github.com>
2023-03-02 12:36:34 +0100

    Update streamlit_app.py
    Added Breakfast menu and three items

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,11 @@ streamlit.header('My Courses')
 streamlit.text('01. EPAM: Data Analytics Engineering')
 streamlit.text('02. IT Academy: FUNDAMENTALS OF BI ANALYTICS')
 streamlit.text('03. DataYoga: QLIK SENSE MARATHON: DATA VISUALIZATION')
+
+
+streamlit.title("My Parrents New Healthy Dinner")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
