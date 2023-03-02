19a43c0494df5ecd65a87f1b9e85ba33f3bb49ca
Eugene <59334320+JenyaYurch@users.noreply.github.com>
2023-03-02 12:45:44 +0100

    Update streamlit_app.py
    Loaded CSV file from that S3 bucket

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,7 @@ streamlit.text('🥑🍞 Avokado Toasts')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.text('🍌 Bananas')
-streamlit.text('🥭 Ananas')
-streamlit.text('Yogurt')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
    
