76bd50b1b61acdbbaf7f3073253e63b0f6e0f3ba
Eugene <59334320+JenyaYurch@users.noreply.github.com>
2023-03-02 12:33:57 +0100

    Update streamlit_app.py
    Added header and three lines of courses

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("Hey there! My name is Eugene, I'm a Junior Data Analytics Engineer")
+
+streamlit.header('My Courses')
+streamlit.text('01. EPAM: Data Analytics Engineering')
+streamlit.text('02. IT Academy: FUNDAMENTALS OF BI ANALYTICS')
+streamlit.text('03. DataYoga: QLIK SENSE MARATHON: DATA VISUALIZATION')
