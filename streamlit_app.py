a93c026fb92e22e48dae1c1ef8f9e3d45761a144
Eugene Yurchenko <jenyayurch@gmail.com>
2023-03-03 16:16:50 +0100

    you can know where the row comes from

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,5 +57,6 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
-
+#  you can know where the row comes from
+my_cur.execute("INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST VALUES ('from streamlit')")
    
