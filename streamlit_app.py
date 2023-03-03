a7c4b2a536510e8a64fe5c52eb994e44dfb5d4b6
Eugene Yurchenko <jenyayurch@gmail.com>
2023-03-03 09:59:12 +0100

    Hidden  writes the data to the screen streamlit.text

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,11 +31,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # New section to display Fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
+ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # just writes the data to the screen
+# streamlit.text(fruityvice_response.json()) # just writes the data to the screen
 
 # take the json version  pf the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
